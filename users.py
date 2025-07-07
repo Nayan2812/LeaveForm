bcdfec12985dd5ef78b7b1eea3f9f71a4482bd32
Nayan2812 <nayanmathur2812@gmail.com>
2025-07-07 13:08:32 +0530

    Bohot kuch

--- users.py
+++ users.py
@@ -2,16 +2,71 @@ USERS = {
     'student1': {
         'id': 'CS001',
         'password': 'pass123',
-        'name': 'Nayan'
+        'name': ''
     },
-    'teacher1': {
-        'id': 'T001',
-        'password': 'teach123',
-        'name': 'Dillip Kumar'
+    "teacher_sugam": {
+        "id": "sugam123",
+        "password": "mentor@123",
+        "name": "Sugam Shivare"
     },
-    'hod1': {
-        'id': 'H001',
-        'password': 'hod123',
+    "teacher_dileep": {
+        "id": "dileep123",
+        "password": "mentor@123",
+        "name": "Dileep Kumar"
+    },
+    "teacher_bagal": {
+        "id": "bagal123",
+        "password": "mentor@123",
+        "name": "Dr. Bagal"
+    },
+    "teacher_rehan": {
+        "id": "rehan123",
+        "password": "mentor@123",
+        "name": "Rehan Ahmed"
+    },
+    "teacher_rajshekhar": {
+        "id": "raj123",
+        "password": "mentor@123",
+        "name": "Prof. Rajshekhar"
+    },
+    "teacher_sachin": {
+        "id": "sachin123",
+        "password": "mentor@123",
+        "name": "Sachin Bhandari"
+    },
+    "teacher_suraj": {
+        "id": "suraj123",
+        "password": "mentor@123",
+        "name": "Suraj Patil"
+    },
+    "teacher_ashok": {
+        "id": "ashok123",
+        "password": "mentor@123",
+        "name": "Dr. Ashok Panigrahi"
+    },
+    "teacher_mohita": {
+        "id": "mohita123",
+        "password": "mentor@123",
+        "name": "Mohita Patel"
+    },
+    "teacher_vishal": {
+        "id": "vishal123",
+        "password": "mentor@123",
+        "name": "Vishal Fegde"
+    },
+    "teacher_sneha": {
+        "id": "sneha123",
+        "password": "mentor@123",
+        "name": "Sneha Yadav"
+    },
+    "teacher_monika": {
+        "id": "monika123",
+        "password": "mentor@123",
+        "name": "Monika Verma"
+    },
+    'hod_govind': {
+        'id': 'govind123',
+        'password': 'hod@123',
         'name': 'HOD BTech/MBA'
     },
     'dean1': {
